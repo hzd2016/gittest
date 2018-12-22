5ad3bf68fc9cd18c503938365e3503743465f61c
huangzd <huangzd@civaonline.cn>
2018-12-22 17:41:39 +0800

    gittest

---
+++ src/main/java/com/haha/demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.haha.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author huangzd
+ * @Date 2018/12/20 15:01
+ */
+@RestController
+public class HelloController {
+    /** 
+    * @Param: [] 
+    * @return: java.lang.String 
+    * @Author: huangzd
+    * @Date: 2018/12/20 
+    */
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello";
+    }
+}
